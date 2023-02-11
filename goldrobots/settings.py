215dfc5ff122ae222d6e0566c90af0ec25597958
mohammadh16 <mohammadh16@yahoo.com>
2023-02-11 18:00:19 +0100

    removeing smtp conf

--- goldrobots/settings.py
+++ goldrobots/settings.py
@@ -143,19 +143,6 @@ MEDIA_URL = '/media/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-#SMTP Configuration
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ashourishakib@gmail.com'
-EMAIL_HOST_PASSWORD = 'qiwpvhxobmzzuykk'
-EMAIL_USE_SSL = False
-
-PASSWORD_RESET_TIMEOUT =14400
-
-
 try:
     from .local_settings import *
 except ImportError:
