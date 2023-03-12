b20cb8a23beee4208df8f416300761c890174fbf
mohammadh16 <mohammadh16@yahoo.com>
2023-03-12 20:13:18 +0100

    removing modeltranslation

--- contract/translation.py
+++
@@ -1,6 +0,0 @@
-from modeltranslation.translator import TranslationOptions,register
-from .models import Contract
-
-@register(Contract)
-class ContractTranslationOptions(TranslationOptions):
-    fields =['name','minimum_duration','minimum_deposit']
